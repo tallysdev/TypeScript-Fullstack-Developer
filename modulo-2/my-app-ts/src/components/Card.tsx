14b593f9ac0e033f31ef9da69afe782ac089a798
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-28 19:22:11 -0300

    :sparkles:feat: First card

---
+++ modulo-2/my-app-ts/src/components/Card.tsx
@@ -0,0 +1,9 @@
+export const Card = () => {
+    return(
+        <div>
+            <h1>TAllys</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore esse eum sequi, explicabo illo architecto cum, repudiandae aut, non delectus debitis. Doloribus maxime nam, quod quaerat quis delectus rem rerum.</p>
+            <p>Deatails about card</p>
+        </div>
+    )
+}
